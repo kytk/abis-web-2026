fae4da33dd48be506881e49d57e300a2acc4d4ed
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2025-10-27 03:55:10 +0900

    add AlizaMS installer

---
+++ code/AlizaMS_setup_mac_abis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/Downloads
+curl -OL https://www.nemotos.net/l4n-abis/macOS_2025/AlizaMS-1.8.3.dmg
+
+DMG_PATH="~/Downloads/AlizaMS-1.8.3.dmg"
+VOLUME=$(hdiutil attach "$DMG_PATH" | grep Volumes | awk '{print $3}')
+echo "AlizaMSをインストールします"
+APP=$(find "$VOLUME" -name "*.app" -maxdepth 1)
+cp -R "$APP" /Applications/
+
+echo "dmgファイルをアンマウントします"
+hdiutil detach "$VOLUME"
+
+echo "AlizaMSのインストールが完了しました。"
+
