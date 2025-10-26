e3a923ec9026e20e7667874c961be848aaa3512e
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2025-10-27 04:59:01 +0900

    update

--- code/AlizaMS_setup_mac_abis.sh
+++ code/AlizaMS_setup_mac_abis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd ~/Downloads
-curl -OL https://www.nemotos.net/l4n-abis/macOS_2025/AlizaMS-1.8.3.dmg
+curl -O https://www.nemotos.net/l4n-abis/macOS_2025/AlizaMS-1.8.3.dmg
+sleep 3
 
 DMG_PATH="~/Downloads/AlizaMS-1.8.3.dmg"
+hdiutil attach $DMG_PATH
 VOLUME=$(hdiutil attach "$DMG_PATH" | grep Volumes | awk '{print $3}')
 echo "AlizaMSをインストールします"
 APP=$(find "$VOLUME" -name "*.app" -maxdepth 1)
